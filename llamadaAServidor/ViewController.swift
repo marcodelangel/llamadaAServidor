9d7780e207535febd41a91513a07f93b9622d04d
marcodelangel <marcodelangel@Marcos-MacBook-Air-2.local>
2016-07-30 17:50:21 -0500

    Borrador1
    Primer borrador.
    ISBN sin guiones.

--- llamadaAServidor/ViewController.swift
+++ llamadaAServidor/ViewController.swift
@@ -8,12 +8,49 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var capturaDeIsbn: UITextField!
+    @IBOutlet weak var cajaMuestraInformacion: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        capturaDeIsbn.delegate = self
+        
     }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        
+        if Reachability.isConnectedToNetwork() == false {
+            print("Internet connection FAILED")
+            let alert = UIAlertController (title: "Alerta",
+                                           message: "No tienes Internet",
+                                           preferredStyle: UIAlertControllerStyle(rawValue: 1)!)
+            let cancelAction = UIAlertAction(title: "Cancel",
+                                             style: UIAlertActionStyle.Default,
+                                             handler: nil)
+            alert.addAction(cancelAction)
+            self.presentViewController(alert, animated: true,
+                                                completion: nil)
+        } else {
+            print("Internet connection OK")
+            var ISBN = capturaDeIsbn.text!
+            ISBN.insert("-", atIndex: ISBN.startIndex.advancedBy(3))
+            ISBN.insert("-", atIndex: ISBN.startIndex.advancedBy(6))
+            ISBN.insert("-", atIndex: ISBN.startIndex.advancedBy(10))
+            ISBN.insert("-", atIndex: ISBN.endIndex.predecessor())
+            let urls = "https://openlibrary.org/api/books?jscmd=data&format=json&bibkeys=ISBN:" + ISBN
+            let url = NSURL(string: urls)
+            let datos:NSData? = NSData(contentsOfURL: url!)
+            let texto = NSString(data: datos!, encoding: NSUTF8StringEncoding)
+            let informacion:NSString = texto!
+            cajaMuestraInformacion.text! = informacion as String
+            capturaDeIsbn.resignFirstResponder()
+        }
+        
+        return true
+    }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
